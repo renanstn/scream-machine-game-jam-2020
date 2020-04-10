64a75324f8c2d58479588b26a6cf02c7e69c9842
Renan Santana <renan_ota@hotmail.com>
2020-04-10 15:04:08 -0300

    Adicionado som ao piscar

--- Dont-Blink/Scripts/Blink.gd
+++ Dont-Blink/Scripts/Blink.gd
@@ -1,6 +1,7 @@
 extends TextureRect
 
 onready var anim_player = $AnimationPlayer
+onready var audio_player = $AudioStreamPlayer
 onready var timer = $Timer
 
 signal time_left(time_left)
@@ -9,15 +10,17 @@ signal max_value(value)
 func _ready():
 	timer.start()
 	emit_signal("max_value", timer.wait_time)
-	
+
 func _process(delta):
 	emit_signal("time_left", timer.time_left)
-
+	if Input.is_action_just_pressed("blink") and not anim_player.is_playing():
+		blink()
 
 func blink():
+	print("blink!")
+	audio_player.play()
 	anim_player.play("Blink")
 	timer.start()
 
 func _on_TestEmitter_test():
-	print("Blink!")
 	blink()
