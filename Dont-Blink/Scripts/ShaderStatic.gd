abfe94b3f6f0649871ca9a8d3a2182c74a29abf2
Renan Santana <renan_ota@hotmail.com>
2020-04-13 20:02:48 -0300

    Primeiro shader finalizado

--- Dont-Blink/Scripts/ShaderStatic.gd
+++ Dont-Blink/Scripts/ShaderStatic.gd
@@ -1,7 +1,13 @@
 extends TextureRect
 
+onready var audio_static = $AudioStatic
+
 func _ready():
 	visible = false
 
 func _on_Blink_shader_static(on):
 	visible = on
+	if on:
+		audio_static.play()
+	else:
+		audio_static.stop()
